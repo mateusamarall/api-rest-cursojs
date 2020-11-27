1f6b979a03f105a8dc1aba16e7a23b17a43e807c
mateusamarall <mateus.amaral018@gmail.com>
2020-11-26 22:05:06 -0300

    gerando seeds pelo sequelize para popular a base de dados

---
+++ src/database/seeds/20201127005446-criar-usuarios.js
@@ -0,0 +1,37 @@
+const bcryptjs = require('bcryptjs');
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => queryInterface.bulkInsert(
+    'users', [
+      {
+        nome: 'luis',
+        email: 'luis1@gmail.com',
+        password_hash: await bcryptjs.hash('123456', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+      {
+        nome: 'luis2',
+        email: 'luis2@gmail.com',
+        password_hash: await bcryptjs.hash('123456789', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+      {
+        nome: 'luis3',
+        email: 'luis3@gmail.com',
+        password_hash: await bcryptjs.hash('12345678910', 8),
+        created_at: new Date(),
+        updated_at: new Date(),
+
+      },
+    ],
+    {},
+  ),
+
+  down: () => {
+
+  },
+};
