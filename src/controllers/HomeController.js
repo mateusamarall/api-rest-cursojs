f5ef15e09ef17a001c797cbdc50e4f28573ab5c5
mateusamarall <mateus.amaral018@gmail.com>
2020-11-27 08:50:40 -0300

    rotas de aluno e gerado nova migration para colocar email como único

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,16 +1,6 @@
-import Aluno from '../models/Aluno';
-
 class HomeController {
   async index(req, res) {
-    const novoAluno = await Aluno.create({
-      nome: 'mateus',
-      sobrenome: 'amaral',
-      email: 'mateus.amaral018@gmail.com',
-      idade: 19,
-      peso: 70,
-      altura: 1.75,
-    });
-    res.json(novoAluno);
+    res.json('index');
   }
 }
 
