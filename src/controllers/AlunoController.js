9b6b89a2edf3f746ecc00e4a04ea594db804e775
mateusamarall <mateus.amaral018@gmail.com>
2020-11-30 09:18:29 -0300

    criado CRUD de alunos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -5,6 +5,89 @@ class AlunoController {
     const alunos = await Aluno.findAll();
     res.json({ alunos });
   }
+
+  async store(req, res) {
+    try {
+      const aluno = await Aluno.create(req.body);
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async show(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      return res.json(aluno);
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async update(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Usuário não encontrado'],
+        });
+      }
+      const novoAluno = await aluno.update(req.body);
+      return res.json(novoAluno);
+    } catch (error) {
+      return res.status(400).json({
+        errors: error.errors.map((err) => err.message),
+      });
+    }
+  }
+
+  async delete(req, res) {
+    try {
+      const { id } = req.params;
+      if (!id) {
+        return res.status(400).json({
+          errors: ['Faltando ID'],
+        });
+      }
+      const aluno = await Aluno.findByPk(id);
+      if (!aluno) {
+        return res.status(400).json({
+          errors: ['Aluno não existe'],
+        });
+      }
+
+      await aluno.destroy();
+
+      return res.json({ message: 'Aluno apagado da base de dados.' });
+    } catch (e) {
+      return res.status(400).json({
+        errors: e.errors.map((err) => err.message),
+      });
+    }
+  }
 }
 
 export default new AlunoController();
