ec9b8469ec77bc529e33e70b03b0279c00d162b7
mateusamarall <mateus.amaral018@gmail.com>
2020-12-11 10:23:53 -0300

    listagem de alunos com suas fotos

--- src/controllers/AlunoController.js
+++ src/controllers/AlunoController.js
@@ -1,9 +1,24 @@
 import Aluno from '../models/Aluno';
+import Foto from '../models/Foto';
 
 class AlunoController {
   async index(req, res) {
-    const alunos = await Aluno.findAll();
-    res.json({ alunos });
+    const alunos = await Aluno.findAll({
+      attributes: [
+        'id',
+        'nome',
+        'sobrenome',
+        'email',
+        'idade',
+        'peso',
+        'altura'],
+      order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+      include: {
+        model: Foto,
+        attributes: ['id', 'filename'],
+      },
+    });
+    res.json(alunos);
   }
 
   async store(req, res) {
@@ -26,7 +41,21 @@ class AlunoController {
           errors: ['Faltando ID'],
         });
       }
-      const aluno = await Aluno.findByPk(id);
+      const aluno = await Aluno.findByPk(id, {
+        attributes: [
+          'id',
+          'nome',
+          'sobrenome',
+          'email',
+          'idade',
+          'peso',
+          'altura'],
+        order: [['id', 'DESC'], [Foto, 'id', 'DESC']],
+        include: {
+          model: Foto,
+          attributes: ['id', 'filename'],
+        },
+      });
       if (!aluno) {
         return res.status(400).json({
           errors: ['Aluno não existe'],
--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -12,10 +12,16 @@ class PhotoController {
           errors: [error.code],
         });
       }
-      const { originalname, filename } = req.file;
-      const { aluno_id } = req.body;
-      const foto = await Foto.create({ originalname, filename, aluno_id });
-      return res.json(foto);
+      try {
+        const { originalname, filename } = req.file;
+        const { aluno_id } = req.body;
+        const foto = await Foto.create({ originalname, filename, aluno_id });
+        return res.json(foto);
+      } catch (e) {
+        return res.status(400).json({
+          errors: ['Aluno não existe.'],
+        });
+      }
     });
   }
 }
