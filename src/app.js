5781f70641e2bc41154a7a9d8b1678cb8363bc8f
mateusamarall <mateus.amaral018@gmail.com>
2021-01-10 18:44:39 -0300

    adicionado cors e helmet

--- src/app.js
+++ src/app.js
@@ -6,12 +6,26 @@ dotenv.config();
 import './database/connection';
 
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
 import alunoRoutes from './routes/alunoRoutes';
 import photoRoutes from './routes/photoRoutes';
 
+const whiteList = [
+  'http://localhost:3000',
+];
+const corsOptions = {
+  origin(origin, callback) {
+    if (whiteList.indexOf[origin] !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+};
 class App {
   constructor() {
     this.app = express();
@@ -20,6 +34,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(cors(corsOptions));
+    this.app.use(helmet());
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static(resolve(__dirname, 'uploads')));
