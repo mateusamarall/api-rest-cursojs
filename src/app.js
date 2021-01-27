413f95bbf2b9f99a8cb9d04821c3ba44e69654d9
mateusamarall <mateus.amaral018@gmail.com>
2021-01-27 19:42:12 -0300

    adicionado express-delay para adicionar atrasso de 2 segundos no carregamento de dados

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ import './database/connection';
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import delay from 'express-delay';
 import homeRoutes from './routes/homeRoutes';
 import userRoutes from './routes/userRoutes';
 import tokenRoutes from './routes/tokenRoutes';
@@ -35,6 +36,7 @@ class App {
   middlewares() {
     this.app.use(cors(corsOptions));
     this.app.use(helmet());
+    this.app.use(delay(2000));
     this.app.use(express.urlencoded({ extended: true }));
     this.app.use(express.json());
     this.app.use(express.static('uploads'));
